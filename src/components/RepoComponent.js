4630e158673a78a8e7129facf63ecb051d07cf17
Greg <g.rutherford10@gmail.com>
2018-10-26 15:02:12 +0100

    initial commit

---
+++ src/components/RepoComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RepoComponent = (props) => {
+
+
+
+  
+
+  return (
+    <h1>Repo's</h1>
+  )
+
+
+}
+
+export default RepoComponent;
---
+++ src/containers/ApiContainer.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import RepoComponent from '../components/RepoComponent';
+
+class ApiContainer extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.loadAPIFunction = this.loadAPIFunction.bind(this);
+
+  }
+
+
+
+  handleChange(event) {
+   this.setState({value: event.target.value});
+ }
+
+ handleSubmit(event) {
+   event.preventDefault();
+   this.loadAPIFunction();
+ }
+
+ loadAPIFunction(){
+    let url = `https://api.github.com/search/repositories?q=${this.state.value}`
+    fetch(url)
+    .then(response => response.json())
+    .then(items => this.setState({items: items}))
+    .catch(err => console.log(err));
+  }
+
+
+
+  render(){
+
+    return (
+      <React.Fragment>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>Repo Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <RepoComponent />
+      </React.Fragment>
+      );
+
+  }
+
+}
+
+export default ApiContainer;
